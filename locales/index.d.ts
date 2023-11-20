8e80ced1b927c85a2ababdafdf03f1944c1bc8be
tmorio <morikapusan@morikapu-denki.com>
2023-11-20 10:05:19 +0900

    投稿したファイルの一覧をプロフィールページ内のタブで見れるようにしてみた (Otaku-Social#14)

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -3491,6 +3491,10 @@ export interface Locale extends ILocale {
      */
     "gallery": string;
     /**
+     * ギャラリー（ノート）
+     */
+    "galleryFromPosts": string;
+    /**
      * 最近の投稿
      */
     "recentPosts": string;
