5c3e229d4f80b2b62e972d4cf87f81c1c918bd2c
水咲(みさき) <60120497+Misaki0331@users.noreply.github.com>
2024-04-11 18:27:27 +0900

    enhance(frontend/MkPostForm): ノートの投稿後すぐに投稿モーダルを閉じるように (MisskeyIO#605)

--- packages/frontend/src/components/MkPostForm.vue
+++ packages/frontend/src/components/MkPostForm.vue
@@ -153,7 +153,9 @@ const props = withDefaults(defineProps<PostFormProps & {
 provide('mock', props.mock);
 
 const emit = defineEmits<{
+	(ev: 'posting'): void;
 	(ev: 'posted'): void;
+	(ev: 'postError'): void;
 	(ev: 'cancel'): void;
 	(ev: 'esc'): void;
 
@@ -897,7 +899,9 @@ async function post(ev?: MouseEvent) {
 			type: 'error',
 			text: err.message + '\n' + (err as any).id,
 		});
+		emit("postError");
 	});
+	emit("posting");
 }
 
 function cancel() {
