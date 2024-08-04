c07d509fdd9955787b79af017c3cb40b38126e60
anatawa12 <anatawa12@icloud.com>
2024-08-04 20:18:45 +0900

    fix: V[RS]TL doesn't have withReplies toggle (#92)
    * fix: V[RS]TL doesn't have withReplies toggle
    
    * docs(changelog): VRTL VSTLでリプライのトグルが表示されない問題

--- packages/frontend/src/timelines.ts
+++ packages/frontend/src/timelines.ts
@@ -52,5 +52,5 @@ export function availableBasicTimelines(): BasicTimelineType[] {
 }
 
 export function hasWithReplies(timeline: BasicTimelineType | undefined | null): boolean {
-	return timeline === 'local' || timeline === 'social';
+	return timeline === 'local' || timeline === 'social' || timeline === 'vmimi-relay' || timeline === 'vmimi-relay-social';
 }
