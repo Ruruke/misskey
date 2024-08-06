c87069f70612609267e1e20ec9ecfd24f40b3d70
dakkar <dakkar@thenautilus.net>
2024-08-06 09:26:40 +0000

    merge: don't send real-time updates of replies to blocked users #457 #573 (!566)
    View MR for information: https://activitypub.software/TransFem-org/Sharkey/-/merge_requests/566
    
    Approved-by: Tess K <me@thvxl.se>
    Approved-by: fEmber <acomputerdog@gmail.com>

--- packages/backend/src/core/NoteCreateService.ts
+++ packages/backend/src/core/NoteCreateService.ts
@@ -8,6 +8,7 @@ import * as mfm from 'mfm-js';
 import { In, DataSource, IsNull, LessThan } from 'typeorm';
 import * as Redis from 'ioredis';
 import { Inject, Injectable, OnApplicationShutdown } from '@nestjs/common';
+import { Data } from 'ws';
 import { extractMentions } from '@/misc/extract-mentions.js';
 import { extractCustomEmojisFromMfm } from '@/misc/extract-custom-emojis-from-mfm.js';
 import { extractHashtags } from '@/misc/extract-hashtags.js';
@@ -57,7 +58,6 @@ import { trackPromise } from '@/misc/promise-tracker.js';
 import { IdentifiableError } from '@/misc/identifiable-error.js';
 import { CollapsedQueue } from '@/misc/collapsed-queue.js';
 import { CacheService } from '@/core/CacheService.js';
-import { Data } from 'ws';
 
 type NotificationType = 'reply' | 'renote' | 'quote' | 'mention';
 
@@ -588,7 +588,7 @@ export class NoteCreateService implements OnApplicationShutdown {
 		if (data.deleteAt) {
 			const delay = data.deleteAt.getTime() - Date.now();
 			this.queueService.scheduledNoteDeleteQueue.add(note.id, {
-				noteId: note.id
+				noteId: note.id,
 			}, {
 				delay,
 				removeOnComplete: true,
