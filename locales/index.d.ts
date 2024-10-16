3c6b58c2a82039c697f7f3a122aeac95c6c752cb
Yunochi <yuno@yunochi.com>
2024-10-16 15:45:16 +0900

    Frontend에 설정 추가

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -2267,6 +2267,14 @@ export interface Locale extends ILocale {
      */
     "objectStoragePrefixDesc": string;
     /**
+     * Prefix For Remote
+     */
+    "objectStoragePrefixForRemote": string;
+    /**
+     * 設定すると、このprefixのディレクトリ下にRemoteファイルが保存されます。
+     */
+    "objectStoragePrefixForRemoteDesc": string;
+    /**
      * Endpoint
      */
     "objectStorageEndpoint": string;
