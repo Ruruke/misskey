032f6f98d8dd8073da2a0f72b854168daa68d08a
ruruke <123709459+Ruruke@users.noreply.github.com>
2024-12-09 00:31:28 +0900

    Update ApImageService.ts

--- packages/backend/src/core/activitypub/models/ApImageService.ts
+++ packages/backend/src/core/activitypub/models/ApImageService.ts
@@ -4,18 +4,17 @@
  */
 
 import { Inject, Injectable } from '@nestjs/common';
+import { UnrecoverableError } from 'bullmq';
 import { DI } from '@/di-symbols.js';
 import type { DriveFilesRepository, MiMeta } from '@/models/_.js';
 import type { MiRemoteUser } from '@/models/User.js';
 import type { MiDriveFile } from '@/models/DriveFile.js';
 import { truncate } from '@/misc/truncate.js';
+import { DB_MAX_IMAGE_COMMENT_LENGTH } from '@/const.js';
 import { DriveService } from '@/core/DriveService.js';
 import type Logger from '@/logger.js';
 import { bindThis } from '@/decorators.js';
 import { checkHttps } from '@/misc/check-https.js';
-import { FederatedInstanceService } from '@/core/FederatedInstanceService.js';
-import type { Config } from '@/config.js';
-import { IdentifiableError } from '@/misc/identifiable-error.js';
 import { ApResolverService } from '../ApResolverService.js';
 import { ApLoggerService } from '../ApLoggerService.js';
 import { isDocument, type IObject } from '../type.js';
@@ -30,13 +29,10 @@ export class ApImageService {
 
 		@Inject(DI.driveFilesRepository)
 		private driveFilesRepository: DriveFilesRepository,
-		@Inject(DI.config)
-		private config: Config,
 
 		private apResolverService: ApResolverService,
 		private driveService: DriveService,
 		private apLoggerService: ApLoggerService,
-		private federatedInstanceService: FederatedInstanceService,
 	) {
 		this.logger = this.apLoggerService.logger;
 	}
@@ -48,7 +44,7 @@ export class ApImageService {
 	public async createImage(actor: MiRemoteUser, value: string | IObject): Promise<MiDriveFile | null> {
 		// 投稿者が凍結されていたらスキップ
 		if (actor.isSuspended) {
-			throw new IdentifiableError('85ab9bd7-3a41-4530-959d-f07073900109', `actor has been suspended: ${actor.uri}`);
+			throw new UnrecoverableError(`actor has been suspended: ${actor.uri}`);
 		}
 
 		const image = await this.apResolverService.createResolver().resolve(value);
@@ -74,19 +70,13 @@ export class ApImageService {
 		// 2. or the image is not sensitive
 		const shouldBeCached = this.meta.cacheRemoteFiles && (this.meta.cacheRemoteSensitiveFiles || !image.sensitive);
 
-		await this.federatedInstanceService.fetch(actor.host).then(async i => {
-			if (i.isNSFW) {
-				image.sensitive = true;
-			}
-		});
-
 		const file = await this.driveService.uploadFromUrl({
 			url: image.url,
 			user: actor,
 			uri: image.url,
-			sensitive: !!(image.sensitive),
+			sensitive: image.sensitive,
 			isLink: !shouldBeCached,
-			comment: truncate(image.name ?? undefined, this.config.maxRemoteAltTextLength),
+			comment: truncate(image.name ?? undefined, DB_MAX_IMAGE_COMMENT_LENGTH),
 		});
 		if (!file.isLink || file.url === image.url) return file;
 
