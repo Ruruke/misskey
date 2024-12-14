6344e02a84f6bfd81b92c0ffdeaf3eb58d1f6a73
ruruke <123709459+Ruruke@users.noreply.github.com>
2024-12-15 00:46:10 +0900

    Update index.d.ts

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -5162,10 +5162,6 @@ export interface Locale extends ILocale {
      * これ以上このクリップにノートを追加できません。
      */
     "clipNoteLimitExceeded": string;
-		/**
-		 * Inboxのモデレーション
-		 */
-		"inboxModeration": string;
     /**
      * パフォーマンス
      */
@@ -5319,6 +5315,10 @@ export interface Locale extends ILocale {
          */
         "resolveTutorial": string;
     };
+    /**
+     * Inboxのモデレーション
+     */
+    "inboxModeration": string;
     "_delivery": {
         /**
          * 配信状態
@@ -6779,102 +6779,112 @@ export interface Locale extends ILocale {
                  */
                 "flavor": string;
             };
+            "_markedAsGorilla": {
+                /**
+                 * うほうほうほうほほ！？？！？！
+                 */
+                "title": string;
+                /**
+                 * アカウントをGorillaとして設定した
+                 */
+                "description": string;
+            };
         };
     };
-		"_inboxRule": {
-			/**
-			 * ルールを追加
-			 */
-			"add": string;
-			/**
-			 * ルール名
-			 */
-			"name": string;
-			/**
-			 * ルールの説明
-			 */
-			"description": string;
-			/**
-			 * このアクティビティが投稿なら
-			 */
-			"thisActivityIsNote": string;
-			/**
-			 * アクション
-			 */
-			"action": string;
-			/**
-			 * だったら
-			 */
-			"then": string;
-			/**
-			 * 拒否
-			 */
-			"reject": string;
-			/**
-			 * このサーバーのユーザーからフォローされているユーザーの数が～以下
-			 */
-			"serverPubLessThanOrEq": string;
-			/**
-			 * このサーバーのユーザーからフォローされているユーザーの数が～以上
-			 */
-			"serverPubMoreThanOrEq": string;
-			/**
-			 * このサーバーのユーザーをフォローしているユーザーの数が～以下
-			 */
-			"serverSubLessThanOrEq": string;
-			/**
-			 * このサーバーのユーザーをフォローしているユーザーの数が～以上
-			 */
-			"serverSubMoreThanOrEq": string;
-			/**
-			 * ノート内のメンション数が～以上
-			 */
-			"maxMentionsMoreThanOrEq": string;
-			/**
-			 * 添付ファイルの数が～以上
-			 */
-			"attachmentFileMoreThanOrEq": string;
-			/**
-			 * この単語を含む
-			 */
-			"isIncludeThisWord": string;
-			/**
-			 * かつ
-			 */
-			"and": string;
-			/**
-			 * または
-			 */
-			"or": string;
-			/**
-			 * でない
-			 */
-			"not": string;
-			/**
-			 * 投稿
-			 */
-			"notes": string;
-			/**
-			 * 条件
-			 */
-			"conditions": string;
-			/**
-			 * 一致した部分を書き換える
-			 */
-			"rewrite": string;
-			/**
-			 * サーバーのホスト
-			 */
-			"serverHost": string;
-			/**
-			 * サーバーのソフトウェア
-			 */
-			"serverSoftware": string;
-			/**
-			 * サーバーサイレンスされているか
-			 */
-			"serverIsSilenced": string;
-		};
+    "_inboxRule": {
+        /**
+         * ルールを追加
+         */
+        "add": string;
+        /**
+         * ルール名
+         */
+        "name": string;
+        /**
+         * ルールの説明
+         */
+        "description": string;
+        /**
+         * このアクティビティが投稿なら
+         */
+        "thisActivityIsNote": string;
+        /**
+         * アクション
+         */
+        "action": string;
+        /**
+         * だったら
+         */
+        "then": string;
+        /**
+         * 拒否
+         */
+        "reject": string;
+        /**
+         * このサーバーのユーザーからフォローされているユーザーの数が～以下
+         */
+        "serverPubLessThanOrEq": string;
+        /**
+         * このサーバーのユーザーからフォローされているユーザーの数が～以上
+         */
+        "serverPubMoreThanOrEq": string;
+        /**
+         * このサーバーのユーザーをフォローしているユーザーの数が～以下
+         */
+        "serverSubLessThanOrEq": string;
+        /**
+         * このサーバーのユーザーをフォローしているユーザーの数が～以上
+         */
+        "serverSubMoreThanOrEq": string;
+        /**
+         * ノート内のメンション数が～以上
+         */
+        "maxMentionsMoreThanOrEq": string;
+        /**
+         * 添付ファイルの数が～以上
+         */
+        "attachmentFileMoreThanOrEq": string;
+        /**
+         * この単語を含む
+         */
+        "isIncludeThisWord": string;
+        /**
+         * かつ
+         */
+        "and": string;
+        /**
+         * または
+         */
+        "or": string;
+        /**
+         * でない
+         */
+        "not": string;
+        /**
+         * 投稿
+         */
+        "notes": string;
+        /**
+         * 条件
+         */
+        "conditions": string;
+        /**
+         * 一致した部分を書き換える
+         */
+        "rewrite": string;
+        /**
+         * サーバーのホスト
+         */
+        "serverHost": string;
+        /**
+         * サーバーのソフトウェア
+         */
+        "serverSoftware": string;
+        /**
+         * サーバーサイレンスされているか
+         */
+        "serverIsSilenced": string;
+    };
     "_role": {
         /**
          * ロールの作成
@@ -10183,22 +10193,6 @@ export interface Locale extends ILocale {
          * ギャラリーの投稿を削除
          */
         "deleteGalleryPost": string;
-				/**
-				 * Inboxのルールを設定
-				 */
-				"setInboxRule": string;
-				/**
-				 * Inboxのルールを削除
-				 */
-				"deleteInboxRule": string;
-				/**
-				 * Inboxで拒否
-				 */
-				"inboxRejected": string;
-				/**
-				 * というルールで拒否されました。
-				 */
-				"rejectedWithTheRule": string;
     };
     "_fileViewer": {
         /**
