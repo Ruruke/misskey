767c3bc539c03176cdb8cadd961e4de600e15dab
ruruke <123709459+Ruruke@users.noreply.github.com>
2024-12-18 21:24:14 +0900

    Update WidgetListenBrainz.vue

--- packages/frontend/src/widgets/WidgetListenBrainz.vue
+++ packages/frontend/src/widgets/WidgetListenBrainz.vue
@@ -6,7 +6,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 <template>
 <MkContainer :showHeader="widgetProps.showHeader" class="mkw-listenBrainz">
 	<template #icon><i class="ti ti-music"></i></template>
-	<template #header>{{ i18n.ts._widgets.listenBrainz }} <span class="_beta">{{ i18n.ts._featureBy.temp }}</span></template>
+	<template #header>{{ i18n.ts._widgets.listenbrainz }} <span class="_beta">{{ i18n.ts._featureBy.temp }}</span></template>
 	<template #func="{ buttonStyleClass }">
 		<button class="_button" :class="buttonStyleClass" @click="fetchPlayingNow()"><i class="ti ti-refresh"></i></button>
 		<button class="_button" :class="buttonStyleClass" @click="configure()"><i class="ti ti-settings"></i></button>
@@ -37,7 +37,7 @@ import { i18n } from '@/i18n.js';
 import { infoImageUrl } from '@/instance.js';
 import { misskeyApi } from '@/scripts/misskey-api.js';
 
-const name = i18n.ts._widgets.listenBrainz;
+const name = i18n.ts._widgets.listenbrainz;
 
 const widgetPropsDef = {
 	showHeader: {
