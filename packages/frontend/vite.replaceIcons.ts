f92bdad6d07a4554daf41e7d0c11decddea06b5c
ruruke <123709459+Ruruke@users.noreply.github.com>
2025-01-05 03:52:55 +0000

    漏れ。

--- packages/frontend/vite.replaceIcons.ts
+++ packages/frontend/vite.replaceIcons.ts
@@ -375,6 +375,7 @@ export function pluginReplaceIcons() {
 				'ti ti-chevron-down': 'ph-caret-down ph-bold ph-lg',
 				'ti ti-bomb': 'ph-bomb ph-bold ph-lg',
 				'ti ti-broadcast': 'ph-broadcast ph-bold ph-lg',
+				'ti ti-inbox': 'ph-tray-arrow-down ph-bold ph-lg',
 			},
 		}),
 	];
