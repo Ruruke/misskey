d80e18a0b89067ba0ebed3c77d1818b9d934c043
ruruke <123709459+Ruruke@users.noreply.github.com>
2025-01-07 16:13:35 +0000

    fix(devcontainer): add multiple git remotes and fetch all

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 set -xe
-
+git remote add sharkey https://activitypub.software/TransFem-org/Sharkey.git
+git remote add shrimpia https://github.com/shrimpia/misskey.git
+git remote add io https://github.com/MisskeyIO/misskey.git
+git remote add upstream https://github.com/misskey-dev/misskey.git
+git remote add youjo https://github.com/yojo-art/cherrypick.git
+git fetch --all
 sudo chown node node_modules
 sudo apt-get update
 sudo apt-get -y install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb
@@ -12,7 +17,6 @@ corepack enable
 pnpm config set store-dir /home/node/.local/share/pnpm/store
 pnpm install --frozen-lockfile
 cp .devcontainer/devcontainer.yml .config/default.yml
-git remote add sharkey https://activitypub.software/TransFem-org/Sharkey.git && git fetch sharkey
 pnpm build
 pnpm migrate
 pnpm exec cypress install
