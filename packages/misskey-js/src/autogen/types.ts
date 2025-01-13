5de7dac82e96785f0906b948586b399a3c0bc5d0
lqvp <183242690+lqvp@users.noreply.github.com>
2025-01-14 07:28:20 +0900

    feat: 連合ページでソフトウェア名で検索出来るように
    misskey-js

--- packages/misskey-js/src/autogen/types.ts
+++ packages/misskey-js/src/autogen/types.ts
@@ -16101,6 +16101,8 @@ export type operations = {
         'application/json': {
           /** @description Omit or use `null` to not filter by host. */
           host?: string | null;
+          /** @description Omit or use `null` to not filter by softwareName. */
+          softwareName?: string | null;
           blocked?: boolean | null;
           notResponding?: boolean | null;
           suspended?: boolean | null;
