168babd6747ef547208062397a6f3eed2ecc9df6
lqvp <183242690+lqvp@users.noreply.github.com>
2025-01-14 07:11:08 +0900

    feat: ノート検索で公開範囲を絞れるように

--- packages/misskey-js/src/autogen/types.ts
+++ packages/misskey-js/src/autogen/types.ts
@@ -24087,6 +24087,11 @@ export type operations = {
            * @default null
            */
           channelId?: string | null;
+          /**
+           * @default all
+           * @enum {string}
+           */
+          visibility?: 'all' | 'public' | 'home' | 'followers' | 'specified';
         };
       };
     };
