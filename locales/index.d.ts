9bc4f989f2352d9ee092494bcd4b068815b5d220
Ruruke <123709459+Ruruke@users.noreply.github.com>
2025-01-27 05:07:54 +0000

    Fix: 通訳漏れ。

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -9988,6 +9988,14 @@ export interface Locale extends ILocale {
              * 連携アプリからの通知
              */
             "app": string;
+            /**
+             * 予約投稿に成功
+             */
+            "scheduledNotePosted": string;
+            /**
+             * 予約投稿に失敗
+             */
+            "scheduledNoteFailed": string;
         };
         "_actions": {
             /**
