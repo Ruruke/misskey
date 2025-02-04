b9cc4ea7bbc66eb659228a2f0975c9dd8c61d601
Ruruke <123709459+Ruruke@users.noreply.github.com>
2025-02-04 04:59:44 +0000

    chore

--- packages/backend/migration/1691264431000-add-lb-to-user.js
+++ packages/backend/migration/1691264431000-add-lb-to-user.js
@@ -1,3 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
 export class AddLbToUser1691264431000 {
 	name = "AddLbToUser1691264431000";
 
