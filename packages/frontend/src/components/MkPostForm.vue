d7e6b22d1a003be19825ab8a3049ee8f499a23f3
ruruke <123709459+Ruruke@users.noreply.github.com>
2025-02-05 20:58:20 +0900

    verbatimModuleSyntax 関係の修正。

--- packages/frontend/src/components/MkPostForm.vue
+++ packages/frontend/src/components/MkPostForm.vue
@@ -106,7 +106,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 </template>
 
 <script lang="ts" setup>
-import { inject, watch, nextTick, onMounted,　onBeforeUnmount, defineAsyncComponent, provide, shallowRef, ref, computed } from 'vue';
+import { inject, watch, nextTick, onMounted, reactive ,onBeforeUnmount, defineAsyncComponent, provide, shallowRef, ref, computed } from 'vue';
 import type { ShallowRef } from 'vue';
 import * as mfm from 'mfm-js';
 import * as Misskey from 'misskey-js';
