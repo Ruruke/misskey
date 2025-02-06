02deb3b21969a140f3325ba0cdcc34c389ce9761
lqvp <183242690+lqvp@users.noreply.github.com>
2025-02-06 12:29:59 +0900

    Update index.d.ts

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -12211,38 +12211,6 @@ export interface Locale extends ILocale {
         "warningText": string;
     };
     /**
-     * すぐ消す
-     */
-    "scheduledNoteDelete": string;
-    /**
-     * このノートは{time}に削除されます
-     */
-    "noteDeletationAt": ParameterizedString<"time">;
-    /**
-     * 1年以上先の日時を指定することはできません
-     */
-    "cannotScheduleLaterThanOneYear": string;
-    /**
-     * 投稿フォーム
-     */
-    "postForm": string;
-    /**
-     * 投稿フォームの下部に表示される項目の並び替えが出来ます。項目をクリックすると削除できます。
-     */
-    "postFormBottomSettingsDescription": string;
-    /**
-     * デフォルトでノートが自己消滅するように
-     */
-    "defaultScheduledNoteDelete": string;
-    /**
-     * 投稿フォームをリセット
-     */
-    "clearPost": string;
-    /**
-     * いいねボタンで使うリアクションを選択
-     */
-    "selectReaction": string;
-    /**
      * いいねボタンを表示する
      */
     "showLikeButton": string;
@@ -12254,14 +12222,6 @@ export interface Locale extends ILocale {
      * リアクションをホバーした際のユーザー一覧と、ノート詳細ページのリアクションタブにあるリアクションをしたユーザー一覧を非表示にします
      */
     "hideReactionUsersDescription": string;
-    /**
-     * リアクション数の非表示
-     */
-    "hideReactionCount": string;
-    /**
-     * 絵文字ピッカーに追加
-     */
-    "addToEmojiPicker": string;
     /**
      * アクティビティを非公開にする
      */
