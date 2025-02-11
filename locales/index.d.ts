65be6b877d6c8d222dedd17917bf30546404f220
おさむのひと <46447427+samunohito@users.noreply.github.com>
2025-02-11 21:05:04 +0900

    enhance(frontend): ノートに埋め込まれたメディアのコンテキストメニューから管理者用のファイル管理画面を開けるように

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -10557,6 +10557,10 @@ export interface Locale extends ILocale {
          */
         "title": string;
         /**
+         * ファイルの詳細(管理者用)
+         */
+        "titleAdmin": string;
+        /**
          * ファイルタイプ
          */
         "type": string;
