ec37f818fdcaf106a8cb9b6595a37f7fd3ac5074
ruru <123709459+Ruruke@users.noreply.github.com>
2025-03-01 23:13:54 +0900

    chore

--- packages/frontend/src/components/MkNote.vue
+++ packages/frontend/src/components/MkNote.vue
@@ -553,7 +553,7 @@ function react(): void {
 			if (defaultStore.state.confirmOnReact) {
 				const confirm = await os.confirm({
 					type: 'question',
-					text: i18n.tsx.reactAreYouSure({ emoji: reaction.replace('@.', '') }),
+					text: i18n.tsx.reactAreYouSure({ emoji: reaction }),
 				});
 
 				if (confirm.canceled) return;
--- packages/frontend/src/components/MkNoteDetailed.vue
+++ packages/frontend/src/components/MkNoteDetailed.vue
@@ -470,7 +470,7 @@ function react(): void {
 			if (defaultStore.state.confirmOnReact) {
 				const confirm = await os.confirm({
 					type: 'question',
-					text: i18n.tsx.reactAreYouSure({ emoji: reaction.replace('@.', '') }),
+					text: i18n.tsx.reactAreYouSure({ emoji: reaction }),
 				});
 
 				if (confirm.canceled) return;
