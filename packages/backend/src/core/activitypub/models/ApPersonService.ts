30c7411b866dafff381cd67618f954a8d8b904c2
syuilo <4439005+syuilo@users.noreply.github.com>
2025-03-01 16:12:42 +0900

    デッドロック解消の試み
    https://github.com/misskey-dev/misskey/issues/15005
    
    Co-authored-by: 饺子w (Yumechi) <35571479+eternal-flame-AD@users.noreply.github.com>

--- packages/backend/src/core/activitypub/models/ApPersonService.ts
+++ packages/backend/src/core/activitypub/models/ApPersonService.ts
@@ -745,10 +745,12 @@ export class ApPersonService implements OnModuleInit {
 		if (moving) updates.movedAt = new Date();
 
 		// Update user
-		const user = await this.usersRepository.findOneByOrFail({ id: exist.id });
+		if (!(await this.usersRepository.update({ id: exist.id, isDeleted: false }, updates)).affected) {
+			return 'skip';
+		}
 		await this.avatarDecorationService.remoteUserUpdate(user);
 		await this.usersRepository.update(exist.id, updates);
-
+		
 		if (person.publicKey) {
 			await this.userPublickeysRepository.update({ userId: exist.id }, {
 				keyId: person.publicKey.id,
@@ -853,7 +855,7 @@ export class ApPersonService implements OnModuleInit {
 
 	@bindThis
 	public async updateFeatured(userId: MiUser['id'], resolver?: Resolver): Promise<void> {
-		const user = await this.usersRepository.findOneByOrFail({ id: userId });
+		const user = await this.usersRepository.findOneByOrFail({ id: userId, isDeleted: false });
 		if (!this.userEntityService.isRemoteUser(user)) return;
 		if (!user.featured) return;
 
