f4b54dd4812ba7292f1b80c275f51f99dc2dd648
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-03-03 17:06:34 +0900

    enhance(frontend): モデレーターがセンシティブ設定を変更する際に確認ダイアログを出すように
    * enhance(frontend): モデレーターがセンシティブ設定を変更する際に確認ダイアログを出すように
    
    * use MkSwitch
    
    * Update Changelog

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -5426,6 +5426,14 @@ export interface Locale extends ILocale {
      * 最も使用された絵文字リアクション上位100件を表示します
      */
     "reactionStatsDescription": string;
+    /**
+     * このメディアをセンシティブとして設定しますか？
+     */
+    "markAsSensitiveConfirm": string;
+    /**
+     * このメディアのセンシティブ指定を解除しますか？
+     */
+    "unmarkAsSensitiveConfirm": string;
     "_accountSettings": {
         /**
          * コンテンツの表示にログインを必須にする
