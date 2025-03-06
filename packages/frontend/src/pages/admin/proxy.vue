4887a48451c19d89199e8067e1df39fbc4ddde5d
ruru <123709459+Ruruke@users.noreply.github.com>
2025-03-07 01:21:02 +0900

    1.2.8

--- packages/frontend/src/pages/admin/proxy.vue
+++ packages/frontend/src/pages/admin/proxy.vue
@@ -5,7 +5,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 
 <template>
 <div>
-	<MkFolder :defaultOpen="true">
+	<MkFolder>
 		<template #icon><i class="ti ti-ghost"></i></template>
 		<template #label>{{ i18n.ts.proxyAccount }}</template>
 		<template v-if="proxyAccountForm.modified.value" #footer>
@@ -14,22 +14,6 @@ SPDX-License-Identifier: AGPL-3.0-only
 
 		<div class="_gaps">
 			<MkInfo>{{ i18n.ts.proxyAccountDescription }}</MkInfo>
-
-			<div class="_panel">
-				<div :class="$style.banner" :style="{ backgroundImage: proxyAccount.bannerUrl ? `url(${ proxyAccount.bannerUrl })` : null }">
-					<MkButton primary rounded :class="$style.bannerEdit" @click="changeProxyAccountBanner">{{ i18n.ts._profile.changeBanner }}</MkButton>
-				</div>
-				<div :class="$style.avatarContainer">
-					<MkAvatar :class="$style.avatar" :user="proxyAccount" forceShowDecoration @click="changeProxyAccountAvatar"/>
-					<div class="_buttonsCenter">
-						<MkButton primary rounded @click="changeProxyAccountAvatar">{{ i18n.ts._profile.changeAvatar }}</MkButton>
-					</div>
-				</div>
-			</div>
-
-			<MkInput v-model="proxyAccountForm.state.name" :max="30" :mfmAutocomplete="['emoji']">
-				<template #label>{{ i18n.ts._profile.name }}</template>
-			</MkInput>
 
 			<MkTextarea v-model="proxyAccountForm.state.description" :max="500" tall mfmAutocomplete :mfmPreview="true">
 				<template #label>{{ i18n.ts._profile.description }}</template>
@@ -83,29 +67,15 @@ const proxyAccountForm = useForm({
 	fetchInstance(true);
 });
 
-function changeProxyAccountAvatar(ev) {
-	selectFile(ev.currentTarget ?? ev.target, i18n.ts.avatar).then(async (file) => {
-		let originalOrCropped = file;
 
-		const { canceled } = await os.confirm({
-			type: 'question',
-			text: i18n.ts.cropImageAsk,
-			okText: i18n.ts.cropYes,
-			cancelText: i18n.ts.cropNo,
+function chooseProxyAccount() {
+	os.selectUser({ localOnly: true }).then(user => {
+		proxyAccount.value = user;
+		os.apiWithDialog('admin/update-meta', {
+			proxyAccountId: user.id,
+		}).then(() => {
+			fetchInstance(true);
 		});
-
-		if (!canceled) {
-			originalOrCropped = await os.cropImage(file, {
-				aspectRatio: 1,
-			});
-		}
-
-		const proxy = await os.apiWithDialog('admin/update-proxy-account', {
-			avatarId: originalOrCropped.id,
-		});
-		proxyAccount.value.avatarId = proxy.avatarId;
-		proxyAccount.value.avatarUrl = proxy.avatarUrl;
-		globalEvents.emit('requestClearPageCache');
 	});
 }
 
