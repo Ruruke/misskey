927e257f22b8fe6e9bbb1dafd6216e5c46f68127
syuilo <4439005+syuilo@users.noreply.github.com>
2025-03-13 19:44:23 +0900

    enhance(frontend): improve preferences

--- packages/frontend/src/store.ts
+++ packages/frontend/src/store.ts
@@ -174,10 +174,6 @@ export const store = markRaw(new Storage('base', {
 		where: 'device',
 		default: {} as Record<string, Record<string, string[]>>,
 	},
-	defaultWithReplies: {
-		where: 'account',
-		default: false,
-	},
 	pluginTokens: {
 		where: 'deviceAccount',
 		default: {} as Record<string, string>, // plugin id, token
@@ -193,6 +189,10 @@ export const store = markRaw(new Storage('base', {
 	},
 
 	//#region TODO: そのうち消す (preferに移行済み)
+	defaultWithReplies: {
+		where: 'account',
+		default: false,
+	},
 	reactions: {
 		where: 'account',
 		default: ['👍', '❤️', '😆', '🤔', '😮', '🎉', '💢', '😥', '😇', '🍮'],
