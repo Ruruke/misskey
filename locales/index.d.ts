0d7be6cdebf90ea162768fe03f7b321beed32b52
syuilo <4439005+syuilo@users.noreply.github.com>
2025-03-13 09:07:22 +0900

    🎨

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -5532,7 +5532,7 @@ export interface Locale extends ILocale {
          */
         "accountData": string;
         /**
-         * アカウントのデータをエクスポート/インポートして管理できます。
+         * アカウントデータのアーカイブをエクスポート/インポートして管理できます。
          */
         "accountDataBanner": string;
         /**
