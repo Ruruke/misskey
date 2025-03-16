e42631b225fe59d3877f86944e4c2880648cc2ca
syuilo <4439005+syuilo@users.noreply.github.com>
2025-03-16 17:48:16 +0900

    enhance(frontend): 投稿フォームの設定メニューを改良 (改)
    This reverts commit a814395127aa1b4ea643fb40235e3e58c747b77f.

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -5514,6 +5514,10 @@ export interface Locale extends ILocale {
      * 投稿フォーム
      */
     "postForm": string;
+    /**
+     * 文字数
+     */
+    "textCount": string;
     "_emojiPalette": {
         /**
          * パレット
