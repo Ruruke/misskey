90828a4e073e99ea642d4066ea585be4f9fb65d7
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-03-31 17:33:00 +0900

    fix(frontend): チャットのデザイン調整 (#15708)
    * fix(frontend): チャットのデザイン調整
    
    * remove unused locales
    
    * 🎨
    
    * Update XMessage.vue
    
    * Update XMessage.vue
    
    ---------
    
    Co-authored-by: syuilo <4439005+syuilo@users.noreply.github.com>

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -5673,7 +5673,7 @@ export interface Locale extends ILocale {
          */
         "thisUserAllowsChatOnlyFromFollowers": string;
         /**
-         * このユーザーはフォローしているユーザーからのみチャットを受け付けています。
+         * このユーザーは、このユーザーがフォローしているユーザーからのみチャットを受け付けています。
          */
         "thisUserAllowsChatOnlyFromFollowing": string;
         /**
