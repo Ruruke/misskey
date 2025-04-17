d0be91061dfcf656a08d883024f36cdde848e47d
syuilo <4439005+syuilo@users.noreply.github.com>
2025-04-17 20:25:59 +0900

    enhance(frontend): improve usability

--- packages/frontend/src/pages/user/home.vue
+++ packages/frontend/src/pages/user/home.vue
@@ -52,110 +52,110 @@ SPDX-License-Identifier: AGPL-3.0-only
 							<span v-if="user.isBot" :title="i18n.ts.isBot"><i class="ti ti-robot"></i></span>
 						</div>
 					</div>
-
-					<template v-if="$i || !user.requireSigninToViewContents">
-						<div v-if="user.followedMessage != null" class="followedMessage">
-							<MkFukidashi class="fukidashi" :tail="narrow ? 'none' : 'left'" negativeMargin shadow>
-								<div class="messageHeader">{{ i18n.ts.messageToFollower }}</div>
-								<div><MkSparkle><Mfm :plain="true" :text="user.followedMessage" :author="user"/></MkSparkle></div>
-							</MkFukidashi>
-						</div>
-						<MkFoldableSection v-if="user.roles.length > 0" class="role-folder" :expanded="user.roles.length < 5">
-							<template #header>{{ i18n.ts.roles }}</template>
-							<div class="roles">
-								<span v-for="role in user.roles" :key="role.id" v-tooltip="role.description" class="role" :style="{ '--color': role.color }">
-									<MkA v-adaptive-bg :to="`/roles/${role.id}`">
-										<img v-if="role.iconUrl" style="height: 1.3em; vertical-align: -22%;" :src="role.iconUrl"/>
-										{{ role.name }}
-									</MkA>
-								</span>
-							</div>
-						</MkFoldableSection>
-						<MkFoldableSection v-if="user.communityRoles.length > 0" class="role-folder" :expanded="user.communityRoles.length < 5">
-							<template #header>{{ i18n.ts.community + " " + i18n.ts.roles }}</template>
-							<div class="roles">
-								<span v-for="role in user.communityRoles" :key="role.id" v-tooltip="role.description" class="role" :style="{ '--color': role.color }">
-									<MkA v-adaptive-bg :to="`/roles/${role.id}`">
-										<img v-if="role.iconUrl" style="height: 1.3em; vertical-align: -22%;" :src="role.iconUrl"/>
-										{{ role.name }}
-									</MkA>
-								</span>
-							</div>
-						</MkFoldableSection>
-						<div v-if="iAmModerator" class="moderationNote">
-							<MkTextarea v-if="editModerationNote || (moderationNote != null && moderationNote !== '')" v-model="moderationNote" manualSave>
-								<template #label>{{ i18n.ts.moderationNote }}</template>
-								<template #caption>{{ i18n.ts.moderationNoteDescription }}</template>
-							</MkTextarea>
-							<div v-else>
-								<MkButton small @click="editModerationNote = true">{{ i18n.ts.addModerationNote }}</MkButton>
-							</div>
-						</div>
-						<div v-if="isEditingMemo || memoDraft" class="memo" :class="{'no-memo': !memoDraft}">
-							<div class="heading" v-text="i18n.ts.memo"/>
-							<textarea
-								ref="memoTextareaEl"
-								v-model="memoDraft"
-								v-autosize
-								rows="1"
-								@focus="isEditingMemo = true"
-								@blur="updateMemo"
-								@input="adjustMemoTextarea"
-							/>
-						</div>
-						<div class="description">
-							<MkOmit>
-								<Mfm v-if="user.description" :text="user.description" :isNote="false" :author="user"/>
-								<p v-else class="empty">{{ i18n.ts.noAccountDescription }}</p>
-								<div v-if="user.description && isForeignLanguage">
-									<MkButton v-if="!(translating || translation)" class="translateButton" small @click="translate"><i class="ti ti-language-hiragana"></i> {{ i18n.ts.translateProfile }}</MkButton>
-									<MkButton v-else class="translateButton" small @click="translation = null"><i class="ti ti-x"></i> {{ i18n.ts.close }}</MkButton>
-								</div>
-							</MkOmit>
-						</div>
-						<div class="fields system">
-							<dl v-if="user.location" class="field">
-								<dt class="name"><i class="ti ti-map-pin ti-fw"></i> {{ i18n.ts.location }}</dt>
-								<dd class="value">{{ user.location }}</dd>
-							</dl>
-							<dl v-if="user.birthday" class="field">
-								<dt class="name"><i class="ti ti-cake ti-fw"></i> {{ i18n.ts.birthday }}</dt>
-								<dd class="value">{{ user.birthday.replace('-', '/').replace('-', '/') }} ({{ i18n.tsx.yearsOld({ age }) }})</dd>
-							</dl>
-							<dl class="field">
-								<dt class="name"><i class="ti ti-calendar ti-fw"></i> {{ user.host === null ? i18n.ts.registeredDate : i18n.ts.registeredAt }}</dt>
-								<dd class="value">{{ dateString(user.createdAt) }} (<MkTime :time="user.createdAt"/>)</dd>
-							</dl>
-						</div>
-						<div v-if="user.fields.length > 0" class="fields">
-							<dl v-for="(field, i) in user.fields" :key="i" class="field">
-								<dt class="name">
-									<Mfm :text="field.name" :author="user" :plain="true" :colored="false"/>
-								</dt>
-								<dd class="value">
-									<Mfm :text="field.value" :author="user" :colored="false"/>
-									<i v-if="user.verifiedLinks.includes(field.value)" v-tooltip:dialog="i18n.ts.verifiedLink" class="ti ti-circle-check" :class="$style.verifiedLink"></i>
-								</dd>
-							</dl>
-						</div>
-						<div class="status">
-							<MkA :to="userPage(user)">
-								<b>{{ number(user.notesCount) }}</b>
-								<span>{{ i18n.ts.notes }}</span>
-							</MkA>
-							<MkA v-if="isFollowingVisibleForMe(user)" :to="userPage(user, 'following')">
-								<b>{{ number(user.followingCount) }}</b>
-								<span>{{ i18n.ts.following }}</span>
-							</MkA>
-							<MkA v-if="isFollowersVisibleForMe(user)" :to="userPage(user, 'followers')">
-								<b>{{ number(user.followersCount) }}</b>
-								<span>{{ i18n.ts.followers }}</span>
-							</MkA>
-						</div>
-					</template>
-					<div v-else class="_fullinfo">
-						<div style="font-size: 1.4rem; font-weight: bold; padding-bottom: 4px;">{{ i18n.ts.pleaseLogin }}</div>
-						<div style="opacity: 0.7">{{ i18n.ts.pleaseLoginToViewProfile }}</div>
+<template v-if="$i || !user.requireSigninToViewContents">
+	<div v-if="user.followedMessage != null" class="followedMessage">
+		<MkFukidashi class="fukidashi" :tail="narrow ? 'none' : 'left'" negativeMargin shadow>
+			<div class="messageHeader">{{ i18n.ts.messageToFollower }}</div>
+			<div><MkSparkle><Mfm :plain="true" :text="user.followedMessage" :author="user" class="_selectable"/></MkSparkle></div>
+		</MkFukidashi>
+	</div>
+	<MkFoldableSection v-if="user.roles.length > 0" class="role-folder" :expanded="user.roles.length < 5">
+		<template #header>{{ i18n.ts.roles }}</template>
+		<div class="roles">
+			<span v-for="role in user.roles" :key="role.id" v-tooltip="role.description" class="role" :style="{ '--color': role.color }">
+				<MkA v-adaptive-bg :to="`/roles/${role.id}`">
+					<img v-if="role.iconUrl" style="height: 1.3em; vertical-align: -22%;" :src="role.iconUrl"/>
+					{{ role.name }}
+				</MkA>
+			</span>
+		</div>
+	</MkFoldableSection>
+	<MkFoldableSection v-if="user.communityRoles.length > 0" class="role-folder" :expanded="user.communityRoles.length < 5">
+		<template #header>{{ i18n.ts.community + " " + i18n.ts.roles }}</template>
+		<div class="roles">
+			<span v-for="role in user.communityRoles" :key="role.id" v-tooltip="role.description" class="role" :style="{ '--color': role.color }">
+				<MkA v-adaptive-bg :to="`/roles/${role.id}`">
+					<img v-if="role.iconUrl" style="height: 1.3em; vertical-align: -22%;" :src="role.iconUrl"/>
+					{{ role.name }}
+				</MkA>
+			</span>
+		</div>
+	</MkFoldableSection>
+	<div v-if="iAmModerator" class="moderationNote">
+		<MkTextarea v-if="editModerationNote || (moderationNote != null && moderationNote !== '')" v-model="moderationNote" manualSave>
+			<template #label>{{ i18n.ts.moderationNote }}</template>
+			<template #caption>{{ i18n.ts.moderationNoteDescription }}</template>
+		</MkTextarea>
+		<div v-else>
+			<MkButton small @click="editModerationNote = true">{{ i18n.ts.addModerationNote }}</MkButton>
+		</div>
+	</div>
+	<div v-if="isEditingMemo || memoDraft" class="memo" :class="{'no-memo': !memoDraft}">
+		<div class="heading" v-text="i18n.ts.memo"/>
+		<textarea
+			ref="memoTextareaEl"
+			v-model="memoDraft"
+			v-autosize
+			rows="1"
+			@focus="isEditingMemo = true"
+			@blur="updateMemo"
+			@input="adjustMemoTextarea"
+		/>
+	</div>
+	<div class="description">
+		<MkOmit>
+			<Mfm v-if="user.description" :text="user.description" :isNote="false" :author="user" class="_selectable"/>
+			<p v-else class="empty">{{ i18n.ts.noAccountDescription }}</p>
+			<div v-if="user.description && isForeignLanguage">
+				<MkButton v-if="!(translating || translation)" class="translateButton" small @click="translate"><i class="ti ti-language-hiragana"></i> {{ i18n.ts.translateProfile }}</MkButton>
+				<MkButton v-else class="translateButton" small @click="translation = null"><i class="ti ti-x"></i> {{ i18n.ts.close }}</MkButton>
+			</div>
+		</MkOmit>
+	</div>
+	<div class="fields system">
+		<dl v-if="user.location" class="field">
+			<dt class="name"><i class="ti ti-map-pin ti-fw"></i> {{ i18n.ts.location }}</dt>
+			<dd class="value">{{ user.location }}</dd>
+		</dl>
+		<dl v-if="user.birthday" class="field">
+			<dt class="name"><i class="ti ti-cake ti-fw"></i> {{ i18n.ts.birthday }}</dt>
+			<dd class="value">{{ user.birthday.replace('-', '/').replace('-', '/') }} ({{ i18n.tsx.yearsOld({ age }) }})</dd>
+		</dl>
+		<dl class="field">
+			<dt class="name"><i class="ti ti-calendar ti-fw"></i> {{ user.host === null ? i18n.ts.registeredDate : i18n.ts.registeredAt }}</dt>
+			<dd class="value">{{ dateString(user.createdAt) }} (<MkTime :time="user.createdAt"/>)</dd>
+		</dl>
+	</div>
+	<div v-if="user.fields.length > 0" class="fields">
+		<dl v-for="(field, i) in user.fields" :key="i" class="field">
+			<dt class="name">
+				<Mfm :text="field.name" :author="user" :plain="true" :colored="false" class="_selectable"/>
+			</dt>
+			<dd class="value">
+				<Mfm :text="field.value" :author="user" :colored="false" class="_selectable"/>
+				<i v-if="user.verifiedLinks.includes(field.value)" v-tooltip:dialog="i18n.ts.verifiedLink" class="ti ti-circle-check" :class="$style.verifiedLink"></i>
+			</dd>
+		</dl>
+	</div>
+	<div class="status">
+		<MkA :to="userPage(user)">
+			<b>{{ number(user.notesCount) }}</b>
+			<span>{{ i18n.ts.notes }}</span>
+		</MkA>
+		<MkA v-if="isFollowingVisibleForMe(user)" :to="userPage(user, 'following')">
+			<b>{{ number(user.followingCount) }}</b>
+			<span>{{ i18n.ts.following }}</span>
+		</MkA>
+		<MkA v-if="isFollowersVisibleForMe(user)" :to="userPage(user, 'followers')">
+			<b>{{ number(user.followersCount) }}</b>
+			<span>{{ i18n.ts.followers }}</span>
+		</MkA>
+	</div>
+</template>
+<div v-else class="_fullinfo">
+	<div style="font-size: 1.4rem; font-weight: bold; padding-bottom: 4px;">{{ i18n.ts.pleaseLogin }}</div>
+	<div style="opacity: 0.7">{{ i18n.ts.pleaseLoginToViewProfile }}</div>
+</div>
 					</div>
 				</div>
 			</div>
