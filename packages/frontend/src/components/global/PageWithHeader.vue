e775cee064b4b50a0744fac61634e1e7dbcfc989
syuilo <4439005+syuilo@users.noreply.github.com>
2025-05-03 20:15:18 +0900

    perf(frontend): tweak PageWithHeader

--- packages/frontend/src/components/global/PageWithHeader.vue
+++ packages/frontend/src/components/global/PageWithHeader.vue
@@ -4,18 +4,18 @@ SPDX-License-Identifier: AGPL-3.0-only
 -->
 
 <template>
-	<div ref="rootEl" :class="[$style.root, reversed ? '_pageScrollableReversed' : '_pageScrollable']">
-		<MkStickyContainer>
-			<template #header><MkPageHeader v-model:tab="tab" v-bind="pageHeaderProps"/></template>
-			<div :class="$style.body">
-				<MkSwiper v-if="swipable && (props.tabs?.length ?? 1) > 1" v-model:tab="tab" :class="$style.swiper" :tabs="props.tabs">
-					<slot></slot>
-				</MkSwiper>
-				<slot v-else></slot>
-			</div>
-			<template #footer><slot name="footer"></slot></template>
-		</MkStickyContainer>
-	</div>
+<div ref="rootEl" :class="[$style.root, reversed ? '_pageScrollableReversed' : '_pageScrollable']">
+	<MkStickyContainer>
+		<template #header><MkPageHeader v-model:tab="tab" v-bind="pageHeaderProps"/></template>
+		<div :class="$style.body">
+			<MkSwiper v-if="prefer.s.enableHorizontalSwipe && swipable && (props.tabs?.length ?? 1) > 1" v-model:tab="tab" :class="$style.swiper" :tabs="props.tabs">
+				<slot></slot>
+			</MkSwiper>
+			<slot v-else></slot>
+		</div>
+		<template #footer><slot name="footer"></slot></template>
+	</MkStickyContainer>
+</div>
 </template>
 
 <script lang="ts" setup>
