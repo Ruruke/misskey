956c451ba9c90e653cb06845ed92753db8f78bf0
syuilo <4439005+syuilo@users.noreply.github.com>
2025-05-07 08:55:42 +0900

    🎨

--- packages/frontend/src/components/index.ts
+++ packages/frontend/src/components/index.ts
@@ -27,6 +27,7 @@ import MkPageHeader from './global/MkPageHeader.vue';
 import MkStickyContainer from './global/MkStickyContainer.vue';
 import MkLazy from './global/MkLazy.vue';
 import MkResult from './global/MkResult.vue';
+import MkSystemIcon from './global/MkSystemIcon.vue';
 import PageWithHeader from './global/PageWithHeader.vue';
 import PageWithAnimBg from './global/PageWithAnimBg.vue';
 import SearchMarker from './global/SearchMarker.vue';
@@ -65,6 +66,7 @@ export const components = {
 	MkStickyContainer: MkStickyContainer,
 	MkLazy: MkLazy,
 	MkResult: MkResult,
+	MkSystemIcon: MkSystemIcon,
 	PageWithHeader: PageWithHeader,
 	PageWithAnimBg: PageWithAnimBg,
 	SearchMarker: SearchMarker,
@@ -97,6 +99,7 @@ declare module '@vue/runtime-core' {
 		MkStickyContainer: typeof MkStickyContainer;
 		MkLazy: typeof MkLazy;
 		MkResult: typeof MkResult;
+		MkSystemIcon: typeof MkSystemIcon;
 		PageWithHeader: typeof PageWithHeader;
 		PageWithAnimBg: typeof PageWithAnimBg;
 		SearchMarker: typeof SearchMarker;
