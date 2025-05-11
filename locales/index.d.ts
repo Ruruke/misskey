860bc8425680c8bba5b9159e0a71e8c6d5dc0a1b
syuilo <4439005+syuilo@users.noreply.github.com>
2025-05-11 14:23:58 +0900

    🎨

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -5882,6 +5882,10 @@ export interface Locale extends ILocale {
          */
         "enableHighQualityImagePlaceholders": string;
         /**
+         * UIのアニメーション
+         */
+        "uiAnimations": string;
+        /**
          * ナビゲーションバーに副ボタンを表示
          */
         "showNavbarSubButtons": string;
