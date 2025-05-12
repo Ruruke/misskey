400391af579389857e69fb8c38a3f450b09c8073
ruru <123709459+Ruruke@users.noreply.github.com>
2025-05-12 21:12:40 +0900

    chore

--- packages/frontend/src/pages/admin/ruru-admin.vue
+++ packages/frontend/src/pages/admin/ruru-admin.vue
@@ -102,7 +102,7 @@ import { definePage } from '@/page.js';
 import MkButton from '@/components/MkButton.vue';
 import FormLink from '@/components/form/link.vue';
 import MkFolder from '@/components/MkFolder.vue';
-import { useForm } from '@/use/use-form.js';
+import { useForm } from '@/composables/use-form.js';
 import MkFormFooter from '@/components/MkFormFooter.vue';
 
 const meta = await misskeyApi('admin/meta');
