de856b277f433910861c8be9a9eaecd050b826ff
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-05-21 21:13:45 +0900

    enhance(frontend): 通知設定ページにサウンド設定ページへのリンクを追加 (#16072)

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -2415,6 +2415,10 @@ export interface Locale extends ILocale {
      */
     "sound": string;
     /**
+     * 通知音の設定
+     */
+    "notificationSoundSettings": string;
+    /**
      * 聴く
      */
     "listen": string;
