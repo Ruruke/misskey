6a97f2ebf7b2543ac48fafb1bd0c43c6b3917647
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-05-22 12:02:01 +0900

    fix(backend): 連合モードが「なし」の場合はactivity jsonへのリンクタグを省略するように (#16074)
    * fix(backend): 連合モードが「なし」の場合はactivity jsonへのリンクタグを省略するように
    
    * Update Changelog
    
    * flip condition

--- packages/backend/src/server/web/ClientServerService.ts
+++ packages/backend/src/server/web/ClientServerService.ts
@@ -215,6 +215,7 @@ export class ClientServerService {
 			instanceUrl: this.config.url,
 			metaJson: htmlSafeJsonStringify(await this.metaEntityService.packDetailed(meta)),
 			now: Date.now(),
+			federationEnabled: this.meta.federation !== 'none',
 		};
 	}
 
