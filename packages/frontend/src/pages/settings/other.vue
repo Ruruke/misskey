6b8a46b7fd6ad4aba5851ba5aa051d0e858f9b7c
syuilo <4439005+syuilo@users.noreply.github.com>
2025-05-22 14:57:35 +0900

    feat(frontend): モバイルデバイスで折りたたまれたUIの展開表示に全画面ページを使用できるように

--- packages/frontend/src/pages/settings/other.vue
+++ packages/frontend/src/pages/settings/other.vue
@@ -86,19 +86,22 @@ SPDX-License-Identifier: AGPL-3.0-only
 					<template #icon><SearchIcon><i class="ti ti-flask"></i></SearchIcon></template>
 					<template #label><SearchLabel>{{ i18n.ts.experimentalFeatures }}</SearchLabel></template>
 
-						<div class="_gaps_m">
-							<MkSwitch v-model="enableCondensedLine">
-								<template #label>Enable condensed line</template>
-							</MkSwitch>
-							<MkSwitch v-model="skipNoteRender">
-								<template #label>Enable note render skipping</template>
-							</MkSwitch>
-							<MkSwitch v-model="stackingRouterView">
-								<template #label>Enable stacking router view</template>
-							</MkSwitch>
-						</div>
-					</MkFolder>
-				</SearchMarker>
+					<div class="_gaps_m">
+						<MkSwitch v-model="enableCondensedLine">
+							<template #label>Enable condensed line</template>
+						</MkSwitch>
+						<MkSwitch v-model="skipNoteRender">
+							<template #label>Enable note render skipping</template>
+						</MkSwitch>
+						<MkSwitch v-model="stackingRouterView">
+							<template #label>Enable stacking router view</template>
+						</MkSwitch>
+						<MkSwitch v-model="enableFolderPageView">
+							<template #label>Enable folder page view</template>
+						</MkSwitch>
+					</div>
+				</MkFolder>
+			</SearchMarker>
 
 			<SearchMarker :keywords="['developer', 'mode', 'debug']">
 				<MkFolder>
@@ -157,6 +160,7 @@ const enableCondensedLine = prefer.model('enableCondensedLine');
 const skipNoteRender = prefer.model('skipNoteRender');
 const devMode = prefer.model('devMode');
 const stackingRouterView = prefer.model('experimental.stackingRouterView');
+const enableFolderPageView = prefer.model('experimental.enableFolderPageView');
 
 watch(skipNoteRender, async () => {
 	await reloadAsk({ reason: i18n.ts.reloadToApplySetting, unison: true });
