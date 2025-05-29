f08bb46bfa79478cf6e420eadd49cb8b0242f96e
syuilo <4439005+syuilo@users.noreply.github.com>
2025-05-29 17:32:10 +0900

    enhance(frontend): デバイスのダークモードと同期するオプションが有効なときにテーマを手動で切り替えようとした際の警告を表示

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -1391,6 +1391,10 @@ export interface Locale extends ILocale {
      */
     "syncDeviceDarkMode": string;
     /**
+     * 「{x}」がオンになっています。同期をオフにして手動でモードを切り替えますか？
+     */
+    "switchDarkModeManuallyWhenSyncEnabledConfirm": ParameterizedString<"x">;
+    /**
      * ドライブ
      */
     "drive": string;
